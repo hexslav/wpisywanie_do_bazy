023d3201fa4ebfeacf2678b5463945225e8bb4c7
Hexslav <lukaszriv@gmail.com>
2017-11-14 09:26:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-2"/>
+    <title>Klienci</title>
+</head>
+
+<body>
+
+    <center>
+
+        <h3>Dodawanie klientów</h3>
+
+        <form method="post" name="form">
+           Podaj id: <input type="number" size="30" name="id"> <br>
+            Podaj imie: <input type="text" size="30" name="imie"> <br>
+            Podaj nazwisko: <input type="text" size="30" name="nazwisko"> <br>
+            Podaj datę urodzenia: <input type="date" size="30" name="data_ur"> <br>
+
+            <input type="submit" name="submit" value="wyślij!" action="zap()">
+        </form>
+
+
+
+    </center>
+
+    <?php
+    $id = $_POST['id'];
+    $imie = $_POST['imie'];
+    $nazwisko = $_POST['nazwisko'];
+    $data_ur = $_POST['data_ur'];
+
+    $p = mysqli_connect('localhost', 'root','','elo') or Die("Nie udało się połączyć z bazą lub serwerem bazy"); 
+    $q = mysqli_query($p,'SELECT * FROM klienci') or Die("błąd zapytania");
+   
+    while($r=mysqli_fetch_assoc($q)){
+    echo "Id: ".$r['id'].".<br>";
+    echo "Imie: ".$r['imie'].".<br>";
+    echo "Nazwisko: ".$r['nazwisko'].".<br>";
+    echo "Data urodzenia: ".$r['data_ur'].".<br>";  
+    }
+  if (isset($_POST['submit'])){
+    $zapytanie = "INSERT INTO klienci VALUES('$id','$imie','$nazwisko','$data_ur')";
+    $wpis = mysqli_query($p, $zapytanie ) or DIE("Błąd wpisu");
+  }
+    mysqli_close($p);
+    
+    ?>
+</body>
+
+</html>
